dc97bb51d02bb04802a80c1eaf976a7a0aa25f7c
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-01-10 00:37:34 -0300

    Criação da entidade de estacionamento, com os VO de CNPJ e endereço.

---
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Domain/Parkings/Parking.cs
@@ -0,0 +1,31 @@
+﻿using ParkingFlow.WebApi.Common.Contracts;
+
+namespace ParkingFlow.WebApi.Domain.Parkings;
+
+public class Parking
+{
+    public Parking(string name, CNPJ cnpj, Address address, string phone, int capacityCar, int capacityMotorcycle)
+    {
+        
+        Guard.IsNotNullOrWhiteSpace(name, nameof(name));
+        Guard.IsNotNullOrWhiteSpace(phone, nameof(phone));
+        Guard.IsGreaterThanOrEqualTo(capacityCar,1 , nameof(capacityCar));
+        Guard.IsGreaterThanOrEqualTo(capacityMotorcycle,1, nameof(capacityMotorcycle));
+        
+        Name = name;
+        CNPJ = cnpj;
+        Address = address;
+        Phone = phone;
+        CapacityCar = capacityCar;
+        CapacityMotorcycle = capacityMotorcycle;
+    }
+
+
+    public string Name { get; private set; } 
+    public CNPJ CNPJ { get; private set; }
+    public Address Address { get; private set; }  
+    public string Phone { get; private set; } 
+    public int CapacityCar { get; private set; }
+    public int CapacityMotorcycle { get; private set; }
+    
+}
