e957897313021b04810c1fc4a74c43a5d5e25ded
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-16 16:55:40 +0000

    Adiciona interface IParkingRepository para gerenciamento de estacionamentos

---
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Domain/Parkings/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingFlow.WebApi.Domain.Parkings;
+
+public interface IParkingRepository
+{
+    void Add(Parking vehicle);
+    void Remove(Parking vehicle);
+    Task<Parking?> GetParkingByIdAsync(Guid id);
+    Task<Parking?> GetParkingByNameAsync(string name);
+    Task<IList<Parking>> ListParkingsByCNPJAsync(string cnpj);
+    Task<bool> ExistsParkingByNameAsync(string name);
+}
