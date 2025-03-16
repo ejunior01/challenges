0fed115dccb1576838e5787ad116a9ecc950f073
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-16 16:56:48 +0000

    Adiciona comandos e manipuladores para criação de estacionamentos

---
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Features/Parkings/Commands/Create/CreateParkingEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using MediatR;
+using ParkingFlow.WebApi.Common.Contracts;
+
+namespace ParkingFlow.WebApi.Features.Parkings.Commands.Create;
+
+public class CreateParkingEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost($"api/v1/{ApiRoutes.Parkings.Create}", Handler)
+            .WithTags("Parkings")
+            .WithSummary("Create parking");
+    }
+
+    private static async Task<IResult> Handler(CreateParkingCommand command, ISender sender,
+        CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(command, cancellationToken);
+
+        return result.IsFailed
+            ? TypedResults.Conflict(result.Errors)
+            : TypedResults.Created($"api/v1/{ApiRoutes.Parkings.Get}/{result.Value.Id}", result.Value);
+    }
+}
---
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Features/Parkings/Commands/Create/CreateParkingHandler.cs
@@ -0,0 +1,29 @@
+﻿using FluentResults;
+using ParkingFlow.WebApi.Common.Abstracts;
+using ParkingFlow.WebApi.Domain.Parkings;
+
+namespace ParkingFlow.WebApi.Features.Parkings.Commands.Create;
+
+public class CreateParkingHandler(IParkingRepository parkingRepository, IUnitOfWork unitOfWork)
+    : ICommandHandler<CreateParkingCommand, Result<Parking>>
+{
+    public async Task<Result<Parking>> Handle(CreateParkingCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var parkingExistWithName = await parkingRepository.ExistsParkingByNameAsync(command.Name);
+
+        if (parkingExistWithName) return Result.Fail(new Error($"Parking {command.Name} already exists"));
+
+        var address = Address.Create(command.Street, command.Number, command.District, command.City, command.State,
+            command.Postcode);
+
+        var cnpj = CNPJ.Create(command.CNPJ);
+
+        var parking = new Parking(command.Name, cnpj, address, command.Phone, command.CapacityCar, command.CapacityMotorcycle);
+
+        parkingRepository.Add(parking);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Ok(parking);
+    }
+}
