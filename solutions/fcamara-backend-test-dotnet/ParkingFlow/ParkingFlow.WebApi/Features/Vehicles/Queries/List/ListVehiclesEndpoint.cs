e7c93a9bb9eeaf3aff0d2e34428fa81abf667712
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-16 16:16:01 +0000

    Adicionado feature de listagem de veículos com suporte à paginação

---
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Features/Vehicles/Queries/List/ListVehiclesEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ParkingFlow.WebApi.Common.Contracts;
+
+namespace ParkingFlow.WebApi.Features.Vehicles.Queries.List;
+public class ListVehiclesEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet($"api/v1/{ApiRoutes.Vehicles.Get}", Handler)
+            .WithTags("Vehicles")
+            .WithSummary("List vehicles");
+    }
+
+    private static async Task<IResult> Handler(
+         ISender sender,
+        CancellationToken cancellationToken,
+        [FromQuery] int page=1,
+        [FromQuery] int pageSize=10
+       )
+    {
+        var query = new ListVehiclesQuery(page, pageSize);
+        var result = await sender.Send(query, cancellationToken);
+
+        return TypedResults.Ok(result.Value);
+    }
+}
