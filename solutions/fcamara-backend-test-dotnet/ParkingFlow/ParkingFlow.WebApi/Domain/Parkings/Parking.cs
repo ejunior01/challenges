8de16439c4a725500806d753d06d047e76443b8a
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-18 00:06:31 -0300

    Refatora classe Parking e adiciona métodos de validação. Substituída a validação direta dos parâmetros no construtor pelo uso do método `ValidateInputs`.

--- solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Domain/Parkings/Parking.cs
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Domain/Parkings/Parking.cs
@@ -4,13 +4,17 @@ namespace ParkingFlow.WebApi.Domain.Parkings;
 
 public class Parking
 {
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public CNPJ CNPJ { get; private set; }
+    public Address Address { get; private set; }
+    public string Phone { get; private set; }
+    public int CapacityCar { get; private set; }
+    public int CapacityMotorcycle { get; private set; }
+
     public Parking(string name, CNPJ cnpj, Address address, string phone, int capacityCar, int capacityMotorcycle)
     {
-
-        Guard.IsNotNullOrWhiteSpace(name, nameof(name));
-        Guard.IsNotNullOrWhiteSpace(phone, nameof(phone));
-        Guard.IsGreaterThanOrEqualTo(capacityCar, 1, nameof(capacityCar));
-        Guard.IsGreaterThanOrEqualTo(capacityMotorcycle, 1, nameof(capacityMotorcycle));
+        ValidateInputs(name, phone, capacityCar, capacityMotorcycle);
 
         Id = Guid.NewGuid();
         Name = name;
@@ -21,12 +25,23 @@ public class Parking
         CapacityMotorcycle = capacityMotorcycle;
     }
 
-    public Guid Id { get; private set; }
-    public string Name { get; private set; }
-    public CNPJ CNPJ { get; private set; }
-    public Address Address { get; private set; }
-    public string Phone { get; private set; }
-    public int CapacityCar { get; private set; }
-    public int CapacityMotorcycle { get; private set; }
+    public void Update(string name, CNPJ cnpj, Address address, string phone, int capacityCar, int capacityMotorcycle)
+    {
+        ValidateInputs(name, phone, capacityCar, capacityMotorcycle);
 
+        Name = name;
+        CNPJ = cnpj;
+        Address = address;
+        Phone = phone;
+        CapacityCar = capacityCar;
+        CapacityMotorcycle = capacityMotorcycle;
+    }
+
+    private static void ValidateInputs(string name, string phone, int capacityCar, int capacityMotorcycle)
+    {
+        Guard.IsNotNullOrWhiteSpace(name, nameof(name));
+        Guard.IsNotNullOrWhiteSpace(phone, nameof(phone));
+        Guard.IsGreaterThanOrEqualTo(capacityCar, 1, nameof(capacityCar));
+        Guard.IsGreaterThanOrEqualTo(capacityMotorcycle, 1, nameof(capacityMotorcycle));
+    }
 }
