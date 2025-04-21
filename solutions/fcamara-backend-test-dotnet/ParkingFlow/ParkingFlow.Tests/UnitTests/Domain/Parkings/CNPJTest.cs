0cec9798acccff891755d1dbb6ca43fc9c4c49ee
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-21 01:13:56 +0000

    Adicionado novos cenários de testes para o VO de CNPJ

--- solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.Tests/UnitTests/Domain/Parkings/CNPJTest.cs
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.Tests/UnitTests/Domain/Parkings/CNPJTest.cs
@@ -5,28 +5,42 @@ namespace ParkingFlow.Tests.UnitTests.Domain.Parkings;
 
 public class CNPJTest
 {
-    [Fact]
-    public void Should_create_cnpj_when_valid_input()
+    [Theory]
+    [InlineData("12.345.678/0001-95")]
+    [InlineData(" 12.345.678/0001-95")]
+    [InlineData("12.345.678/0001-95 ")]
+    [InlineData("12345678000195")]
+    [InlineData(" 12345678000195")]
+    [InlineData("12345678000195 ")]
+    public void Should_create_cnpj_when_valid_input(string input)
     {
-        var cnpj = CNPJ.Create("12.345.678/0001-95");
+        var cnpj = CNPJ.Create(input);
 
         cnpj.Value.Should().NotBeNullOrEmpty();
     }
 
     [Theory]
-    [InlineData("AAA")]
-    [InlineData("1234")]
-    [InlineData("12.345.678/000195")]
-    [InlineData("12.345.678/0001-095")]
-    [InlineData("1234567800019500")]
+    [InlineData("12.345.678/0001-9")]
+    [InlineData("12.345.678/0001-950")]
+    [InlineData("12.345.678/0001950")]
+    [InlineData("12.345.6780001-950")]
+    [InlineData("12.345678/0001-950")]
+    [InlineData("12345.678/0001-950")]
+    [InlineData("12345.6780001-950")]
+    [InlineData("12.345.678/0001.95")]
+    [InlineData("12.345.678/0001#95")]
+    [InlineData("12.345@678/0001#95")]
+    [InlineData("12.345_678/0001#95")]
+    [InlineData("123456780001950")]
+    [InlineData("1234567800019")]
     [InlineData("")]
     [InlineData(" ")]
     [InlineData(null)]
-    public void Should_throw_exception_when_creating_plate_with_invalid_value(string value)
+    public void Should_throw_exception_when_creating_plate_with_invalid_value(string input)
     {
         var act = () =>
         {
-            var _ = CNPJ.Create(value);
+            var _ = CNPJ.Create(input);
 
         };
 
