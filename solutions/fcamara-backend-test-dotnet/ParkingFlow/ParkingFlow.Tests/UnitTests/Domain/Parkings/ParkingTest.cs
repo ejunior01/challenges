0271514c9faeb8e05f34cdd9e5c5670cb1ecb658
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-21 01:14:35 +0000

    Adicionado novos cenários de teste para a entidade de estacionamento

--- solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.Tests/UnitTests/Domain/Parkings/ParkingTest.cs
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.Tests/UnitTests/Domain/Parkings/ParkingTest.cs
@@ -16,4 +16,23 @@ public class ParkingTest
         parking.Name.Should().Be("Shopping Acme");
 
     }
+
+    [Theory]
+    [InlineData("Shopping Acme", "1105150215", 15, 0, typeof(ArgumentOutOfRangeException))]
+    [InlineData("Shopping Acme", "1105150215", 0, 10, typeof(ArgumentOutOfRangeException))]
+    [InlineData(null, "1105150215", 15, 2, typeof(ArgumentNullException))]
+    public void Should_throw_exception_when_creating_parking_with_invalid_values(string name, string phone, int capacityCar, int capacityMotorcycle, Type  exceptionType)
+    {
+
+        var cnpj = CNPJ.Create("12.345.678/0001-95");
+        var address = Address.Create("R. dos Estudantes", "37", "Liberdade", "São Paulo", "SP", "01505-000");
+
+        var act = () =>
+        {
+            var _ = new Parking(name, cnpj, address, phone, capacityCar, capacityMotorcycle);
+
+        };
+
+        act.Should().Throw<Exception>().Which.Should().BeOfType(exceptionType);
+    }
 }
