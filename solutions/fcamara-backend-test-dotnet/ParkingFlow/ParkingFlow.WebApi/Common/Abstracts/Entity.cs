8cdae75f089e28307e7fdc5fddb408dd94f9b517
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-04-22 23:16:10 -0300

    adicionado na classe Entity a propriedade Id e  mplementa a interface IEquatable<Entity> permitindo comparações de igualdade entre instâncias

--- solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Common/Abstracts/Entity.cs
+++ solutions/fcamara-backend-test-dotnet/ParkingFlow/ParkingFlow.WebApi/Common/Abstracts/Entity.cs
@@ -1,5 +1,38 @@
 ﻿namespace ParkingFlow.WebApi.Common.Abstracts;
-
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
+    protected Entity() => Id = Guid.NewGuid();
+    public Guid Id { get; private set; }
+
+    public override bool Equals(object obj) => Equals(obj as Entity);
+
+    public bool Equals(Entity? other)
+    {
+        if (other is not Entity entity)
+            return false;
+
+        return Id.Equals(entity.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity left, Entity right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity left, Entity right)
+    {
+        return !(left == right);
+    }
+
 }
